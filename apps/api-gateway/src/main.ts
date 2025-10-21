efbc1fb171bf2b77b51eb89ab6ead1acadc1692a
Zerferrous <matvey.dubkov.05@gmail.com>
2025-10-21 20:46:09 +0300

    added simple docker-compose configuration

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
   await app.listen(process.env.port ?? 3000);
+  Logger.log(`API-GATEWAY started on port ${process.env.port ?? 3000}`);
 }
 bootstrap();
